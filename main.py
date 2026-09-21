b19a48dd5739e31e3cd39aedd36aa6c960ab0656
kodapesujal36-spec <kodapesujal36@gmail.com>
2026-09-21 22:18:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    app.run_polling()  # If you use polling
+# Don't use app.run_polling() on Render Web Service
+# Use this:
+import threading
+
+def run_bot():
+    app.run_polling()
+
+threading.Thread(target=run_bot).start()
+app_flask.run(host='0.0.0.0', port=10000)
